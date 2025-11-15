a45944b3f62de9dd08950d86d10052b2b4e853dc
StanislavDidus <stasdidus9@gmail.com>
2025-11-15 19:43:30 +0100

    Floating point compatability

--- Enhanced Calculator/main.cpp
+++ Enhanced Calculator/main.cpp
@@ -57,7 +57,7 @@ int main()
 		char sign = ' ';
 
 		//If the incoming symbols is an operand, print it..
-		if (expression[i] >= '0' && expression[i] <= '9')
+		if (isNumber(expression[i]) || expression[i] == '.')
 		{
 			std::string num = "";
 
@@ -66,7 +66,7 @@ int main()
 			{
 				num += expression[nextSymbol];
 				nextSymbol++;
-			} while (nextSymbol < expression.length() && isNumber(expression[nextSymbol]));
+			} while (nextSymbol < expression.length() && (isNumber(expression[nextSymbol]) || expression[nextSymbol] == '.'));
 
 			number = std::stof(num);
 
