3c34ef59e039f7f2a9f20567e8b96646dd790779
StanislavDidus <stasdidus9@gmail.com>
2025-12-18 18:03:39 +0100

    Optimized isPrime function

--- Prime Number/main.cpp
+++ Prime Number/main.cpp
@@ -21,7 +21,7 @@ bool isPrime(int number)
 	if (number <= 1) return false;
 	//if (number == 2) return true;
 	
-	for (int i = 2; i < number; i++)
+	for (int i = 2; i * i <= number; i++)
 	{
 		if (i != number && number % i == 0) return false;
 	}
@@ -29,9 +29,25 @@ bool isPrime(int number)
 	return true;
 }
 
+bool isPrimeOptimized(int number)
+{
+	if (number <= 1) return false;
+	if (number <= 3) return true;
+
+	if (number % 2 == 0 || number % 3 == 0) return false;
+
+	for (int i = 5; i * i <= number; i += 6)
+	{
+		if (number % i == 0 || number % (i + 2) == 0) return false;
+	}
+
+	return true;
+}
+
 int main()
 {
-	std::cout << "Enter a natural number" << std::endl;
+	//1
+	/*std::cout << "Enter a natural number" << std::endl;
 
 	int number = readNumber();
 
@@ -42,6 +58,21 @@ int main()
 	else
 	{
 		std::cout << "<" << number << ">" << " is not prime." << std::endl;
+	}*/
+
+	//2
+	std::cout << "Enter min value" << std::endl;
+	int min = readNumber();
+
+	std::cout << "Enter max value" << std::endl;
+	int max = readNumber();
+
+	for (int i = min; i < max; ++i)
+	{
+		if (isPrimeOptimized(i))
+		{
+			std::cout << i << " ";
+		}
 	}
 
 	return 0;
