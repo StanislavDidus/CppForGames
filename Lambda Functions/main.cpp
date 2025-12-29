2182951fe5e9dd6bc906c80e3b52c811d8ec014e
StanislavDidus <stasdidus9@gmail.com>
2025-12-29 21:22:16 +0100

    Lambda Functions

---
+++ Lambda Functions/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> vec{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	vec.erase(std::remove_if(vec.begin(), vec.end(), [](int num) { return num % 2 != 0; }), vec.end());
+
+	for (int i = 0; i < vec.size(); i++)
+	{
+		std::cout << "Value <" << i << "> is: " << vec[i] << std::endl;
+	}
+
+	return 0;
+}
