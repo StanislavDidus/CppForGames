6d12ccfbf7ac9474ef1e7929f170b1565bf5b819
StanislavDidus <stasdidus9@gmail.com>
2026-01-13 21:23:20 +0100

    Dynamic array

---
+++ Dynamic Array Template/inc/Array.hpp
@@ -0,0 +1,147 @@
+#pragma once
+
+template<typename T>
+class Array
+{
+public:
+	Array() : m_capacity(8)
+	{
+		m_data = new T[m_capacity];
+	}
+
+	Array(size_t size, const T& value = T()) : m_size(size), m_capacity(size)
+	{
+		m_data = new T[size];
+
+		for (size_t i = 0; i < size; ++i)
+		{
+			m_data[i] = value;
+		}
+	}
+
+	~Array()
+	{
+		delete[] m_data;
+	}
+
+	//Copy Semantics
+	Array(const Array& copy);
+	Array& operator=(const Array& rhs);
+
+	//Move Semantics
+	Array(Array&& other);
+	Array& operator=(Array&& rhs);
+
+	//Subscript operator
+	template<typename Self>
+	auto&& operator[](this Self&& self, size_t i);
+
+	void reserve(size_t new_capacity)
+	{
+		T* new_data = new T[new_capacity];
+	
+		for (size_t i = 0; i < m_size; ++i)
+		{
+			new_data[i] = std::move(m_data[i]);
+		}
+
+		delete[] m_data;
+		m_data = new_data;
+		m_capacity = new_capacity;
+	}
+
+	void add(const T& value)
+	{
+		if (m_size >= m_capacity) reserve(m_capacity * 2);
+
+		m_data[m_size] = value;
+		m_size++;
+	}
+
+	bool empty() const
+	{
+		return m_size == 0;
+	}
+
+	size_t size() const
+	{
+		return m_size;
+	}
+
+	size_t capacity() const
+	{
+		return m_capacity;
+	}
+
+	template<typename Self>
+	auto&& data(this Self&& self);
+
+private:
+	T* m_data = nullptr;
+	size_t m_size = 0;
+	size_t m_capacity = 0;
+};
+
+template<typename T>
+Array<T>::Array(const Array& copy) : m_size(copy.m_size), m_capacity(copy.m_capacity)
+{
+	m_data = new T[m_size];
+	std::memcpy(m_data, copy.m_data, m_size * sizeof(T));
+}
+
+template<typename T>
+Array<T>& Array<T>::operator=(const Array<T>& rhs)
+{
+	if (this == &rhs) return *this;
+
+	if (m_size != rhs.m_size)
+	{
+		delete[] m_data;
+		m_size = rhs.m_size;
+		m_capacity = rhs.m_capacity;
+		m_data = new T[m_size];
+	}
+	std::memcpy(m_data, rhs.m_data, m_size * sizeof(T));
+
+	return *this;
+}
+
+template<typename T>
+Array<T>::Array(Array&& other) : m_size(other.m_size), m_capacity(other.m_capacity)
+{
+	m_data = other.m_data;
+
+	other.m_size = 0;
+	other.m_data = nullptr;
+	other.m_capacity = 0;
+}
+
+template<typename T>
+Array<T>& Array<T>::operator=(Array<T>&& rhs)
+{
+	if (this == &rhs) return *this;
+
+	m_size = rhs.m_size;
+	m_data = rhs.m_data;
+
+	rhs.m_data = nullptr;
+	rhs.m_size = 0;
+
+	return *this;
+}
+
+template<typename T>
+template<typename Self>
+auto&& Array<T>::operator[](this Self&& self, size_t i)
+{
+	assert(i < self.m_size);
+	return self.m_data[i];
+
+}
+
+template<typename T>
+template<typename Self>
+auto&& Array<T>::data(this Self&& self)
+{
+	return self.m_data;
+}
---
+++ Dynamic Array Template/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cassert>
+
+#include <Array.hpp>
+
+int main()
+{
+	//Default constructor
+	{
+		Array<float> vector;
+
+		assert(vector.size() == 0 && vector.capacity() == 8);
+	}
+
+	//Parameterized constructor
+	{
+		Array<float> vector{ 25, 100.f };
+		assert(vector.size() == 25);
+		for (size_t i = 0; i < 25; ++i)
+		{
+			assert(vector.data()[i] == 100.f);
+		}
+	}
+
+	//Copy constructor
+	{
+		Array<float> vector{ 10,10.f };
+		Array<float> vector2{ vector };
+
+		for (size_t i = 0; i < vector.size(); ++i)
+		{
+			assert(vector[i] == vector2[i]);
+		}
+	}
+
+	//Move constructor
+	{
+		Array<float> vector{ 5, 7.f };
+		Array<float> vector2{ std::move(vector) };
+		
+		assert(vector2.size() == 5 && vector.empty());
+	}
+
+	//Copy assignment
+	{
+		Array<float> vector{ 3, 5.f };
+		Array<float> vector2 = vector;
+
+		for (size_t i = 0; i < vector.size(); ++i)
+		{
+			assert(vector[i] == vector2[i]);
+		}
+	}
+
+	//Move assignment
+	{
+		Array<float> vector{ 3, 5.f };
+		Array<float> vector2 = std::move(vector);
+
+		assert(vector2[0] == 5.f && vector.empty());
+	}
+
+	return 0;
+}
